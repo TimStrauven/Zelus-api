e848220a9aa9ee42ab5a9141ef8e06fe3adeaf9f
Tim Strauven <tim.strauven@gmail.com>
2022-06-10 11:38:44 +0200

    Updated javascript and fastapi to allow receiving picture from camera

--- Utils/routes.py
+++ Utils/routes.py
@@ -1,15 +1,18 @@
 
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import HTMLResponse, StreamingResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.requests import Request
 from typing import List, Dict
 import os
-
+from PIL import Image
+import base64
+from io import BytesIO
 
 class Routes:
     app = FastAPI()
+    # CORS(app)
 
     def __init__(self):
         self.app.mount("/static", StaticFiles(directory="./static"), name="static")
@@ -51,6 +54,35 @@ class Routes:
             return templates.TemplateResponse("type.html", context)
 
         @app.get("/camera", response_class=HTMLResponse)
-        async def camera(request: Request):
+        async def camera_get(request: Request):
             context = {"request": request}
             return templates.TemplateResponse("camera.html", context)
+
+        @app.post("/camera_post", response_class=HTMLResponse)
+        async def camera_post(file: UploadFile = File(...)):
+            if not file:
+                print("No image")
+            else:
+                #dataURL = bytes(image, 'utf-8')
+                image = await file.read()
+                with open("temp.jpg", "wb") as fh:
+                    fh.write(image)
+
+                #im = Image.open("temp.jpg")
+                print("image is here")
+
+            return templates.TemplateResponse("camera.html")
+
+        @app.post("/upload")
+        async def upload(image: UploadFile = File(...)):
+            try:
+                contents = await image.read()
+                with open("uploaded_" + image.filename, "wb") as f:
+                    f.write(contents)
+            except Exception:
+                return {"message": "There was an error uploading the file"}
+            finally:
+                await image.close()
+
+            return {"message": f"Successfuly uploaded {image.filename}"}
+
--- static/app.js
+++ static/app.js
@@ -22,15 +22,48 @@ cameraTrigger.onclick = function() {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.src = cameraSensor.toDataURL("image/jpeg");
     cameraOutput.classList.add("taken");
-
-    $.post( "/", {
-        image: cameraOutput.src
-      });
+    //uploadFile(cameraOutput.src);
+    var imagefile = dataURLtoFile(cameraOutput.src, "image.jpeg");
+    uploadFile(imagefile);
+    //uploadFile("/home/becode2/Desktop/Zelus_Images/archive/archive_cleaned/vinted_test_only/dresses/1367721197.jpeg");
     setTimeout(function() {
         location.reload();
     }, 3000);
 };
+
+function uploadFile(file){
+    if(file){
+        var xhr = new XMLHttpRequest();
+        var url = "/upload";
+        xhr.open("POST", url, true);
+        // xhr.setRequestHeader("Content-Type", "multipart/form-data");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                console.log(xhr.responseText);
+            }
+        };
+        var formData = new FormData();
+        formData.append("image", file);
+        xhr.send(formData);
+    }
+}
+
+function dataURLtoFile(dataurl, filename) {
+ 
+    var arr = dataurl.split(','),
+        mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]), 
+        n = bstr.length, 
+        u8arr = new Uint8Array(n);
+        
+    while(n--){
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    
+    return new File([u8arr], filename, {type:mime});
+}
+
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
